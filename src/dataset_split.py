2914189b1c88881f42207e0de0010680c744ade8
Dhay <Diego50094@gmail.com>
2025-01-22 01:29:56 -0300

    Update dataset split to enforce complete balance between attack types

--- src/dataset_split.py
+++ src/dataset_split.py
@@ -25,16 +25,27 @@ def SplitDataset(df_day_1: pd.DataFrame, df_day_2: pd.DataFrame, rs: RandomState
     # Para validação, queremos os demais dados benignos e os dados malignos do segundo dia
     df_val_mal = df_day_2[df_day_2["Label"].isin(["Syn", "UDP", "UDPLag", "MSSQL", "NetBIOS", "LDAP"])]
     df_val_ben = df_day_2.drop(df_train.index)
-    df_val_ben = df_val_ben[df_val_ben["Label"] == BENIGN].sample(
-        n=min(df_val_ben[df_val_ben["Label"] == BENIGN].__len__(), 11342), random_state=rs
-    )
+    df_val_ben = df_val_ben[df_val_ben["Label"] == BENIGN]
     df_val = pd.concat([df_val_ben, df_val_mal])
     
     
     # Para o teste, queremos os dados benignos do primeiro dia e uma quantidade igual de dados malignos,
     # com os mesmos ataques usados anteriormente + Portmap
-    # ALERTA: Dados estão rotulados de forma diferente entre os dias
-    df_test_mal = df_day_1[df_day_1["Label"].isin(["Syn", "UDP", "UDPLag", "MSSQL", "NetBIOS", "LDAP", "Portmap"])]
+    # ALERTA: Dados estão rotulados de forma diferente entre os dias no dataset original, porém isso foi corrigido
+    attack_types = ["Syn", "UDP", "UDPLag", "MSSQL", "NetBIOS", "LDAP", "Portmap"]
+    df_test_mal = pd.DataFrame()
+    for at in attack_types:
+        max_entries = 8021
+        if at == "Portmap":
+            max_entries = 8022
+            
+        df_test_mal = pd.concat([
+            df_test_mal,
+            df_day_1[df_day_1["Label"] == at].sample(
+                n=min(df_day_1[df_day_1["Label"] == at].__len__(), max_entries), 
+                random_state=rs
+                )
+            ])
     
     df_test_ben = df_day_1[df_day_1['Label']==BENIGN].sample(
         n=min(df_day_1[df_day_1['Label']==BENIGN].__len__(), 50000, df_test_mal.__len__()), random_state=rs
