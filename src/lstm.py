a26963ac38b4c76a9ddab25e62b8432a8761e516
Ricardo Bizerra <ricardo.bizerra19@gmail.com>
2025-03-13 19:40:14 +0000

    fix: correct arguments passed to lstm

--- src/lstm.py
+++ src/lstm.py
@@ -1,9 +1,9 @@
 import torch.nn as nn
 
 class LSTM(nn.Module):
-    def __init__(self, input_dim, hidden_dim, layer_dim, output_dim):
+    def __init__(self, input_dim, hidden_dim):
         super(LSTM, self).__init__()
-        self.lstm = nn.LSTM(input_dim=input_dim, hidden_dim=hidden_dim, layer_dim=layer_dim, num_layers=1, batch_first=True)
+        self.lstm = nn.LSTM(input_size=input_dim, hidden_size=hidden_dim, num_layers=1, batch_first=True)
     
     def forward(self, x):
         out, _ = self.lstm(x)
