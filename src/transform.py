995e308bd8d9f99d0a712f70f3dd9b2a9a500bf2
Dhay <Diego50094@gmail.com>
2025-03-22 14:54:14 -0300

    fix: set device in transform

--- src/transform.py
+++ src/transform.py
@@ -10,6 +10,7 @@ class MeanNormalizeTensor:
 
     @torch.no_grad
     def __call__(self, tensor):
+        tensor = tensor.to(device)
         return (tensor - self.mean) / (self.std + 1e-9)
 
 class MinMaxNormalizeTensor:
@@ -19,4 +20,5 @@ class MinMaxNormalizeTensor:
     
     @torch.no_grad
     def __call__(self, tensor):
+        tensor = tensor.to(device)
         return (tensor - self.min) / (self.max - self.min + 1e-9)
